eb2aebc93c1dbc9e1363c4bccf618204d8dfb985
sonyjgomes <sonygcccul@gmail.com>
2018-09-16 13:29:42 +0600

    first commit

---
+++ Mvc for bit test/Mvc for bit test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mvc_for_bit_test.MvcApplication" Language="C#" %>
---
+++ Mvc for bit test/Mvc for bit test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc_for_bit_test.Startup))]
+namespace Mvc_for_bit_test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
